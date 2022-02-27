8297c29cf40b7006ea605f6378c8de14fbc9748d
Thomas Gibbons <tjgibbons5906@gmail.com>
2022-02-27 03:46:16 -0600

    connect browser to wallet

--- src/App.js
+++ src/App.js
@@ -1,28 +1,91 @@
-import twitterLogo from './assets/twitter-logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-// Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = '419NFT';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const TWITTER_LOGO = 'https://pbs.twimg.com/profile_banners/1408984211101523969/1643282611/600x200'
 
+// Main component launched from index.js by ReactDOM.render
 const App = () => {
+  // State
+  const [walletAddress, setWalletAddress] = useState(null);
+  
+  // Function that checks if solana wallet is connected
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
+
+      if (solana) {
+        console.log('Solana wallet found');
+        if (solana.isPhantom) {
+          console.log('(Phantom wallet)')
+        }
+        const response = await solana.connect({ onlyIfTrusted: true });
+        console.log(
+          'Connected with Public Key:',
+          response.publicKey.toString()
+        );
+        setWalletAddress(response.publicKey.toString());
+      } else {
+        console.log('Solana wallet not found')
+        alert('Solana object not found! Get a Phantom Wallet 👻');
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // Call the check wallet connected function once on component mount
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad);
+  }, []);
+
+  // Function to connect wallet
+  const connectWallet = async () => {
+    const { solana } = window;
+
+    if (solana) {
+      const response = await solana.connect();
+      console.log('Connected with Public Key:', response.publicKey.toString());
+      setWalletAddress(response.publicKey.toString());
+    } else {
+      console.log('No solana wallet')
+    }
+  };
+
+  // redner UI if wallet not yet connected
+  const renderNotConnectedContainer = () => (
+    <button
+      className="cta-button connect-wallet-button"
+      onClick={connectWallet}
+    >
+      Connect to Wallet
+    </button>
+  );
+
+  // main body of page
   return (
     <div className="App">
-      <div className="container">
+      <div className={walletAddress ? 'authed-container' : 'container'}>
         <div className="header-container">
-          <p className="header">🖼 GIF Portal</p>
+          <p className="header">Monke Words of Wisdom</p>
           <p className="sub-text">
-            View your GIF collection in the metaverse ✨
+            Hear the words of the wise monkes of 419NFT
           </p>
+          {!walletAddress && renderNotConnectedContainer()}
         </div>
         <div className="footer-container">
-          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+          <img alt="Twitter Logo" className="twitter-logo" src={TWITTER_LOGO} />
           <a
             className="footer-text"
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+          >{TWITTER_HANDLE}</a>
         </div>
       </div>
     </div>
